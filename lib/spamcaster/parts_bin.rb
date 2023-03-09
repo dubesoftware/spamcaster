c2d4eeba2079c8ea9d4eec59e063d6824b57248b
dubesoftware <therealdubesoftware@gmail.com>
2023-03-09 19:36:06 +0200

    Create parts_bin.rb config

---
+++ lib/spamcaster/parts_bin.rb
@@ -0,0 +1,13 @@
+module Spamcaster
+  module PartsBin
+    SLK_WHK_SUBDOMAIN = 'hooks'
+    SLK_WHK_DOMAIN    = 'slack'
+    SLK_WHK_EXTENSION = '.com'
+    SLK_WHK_PATH      = 'services'
+    SLK_WHK_REMAINDER = {
+      1 => 'TF0RCGM3M',
+      2 => 'B04TWV2A9TJ'
+      3 => 'Z6bAvFPsERSZm6VIPvbONHtr'
+    }
+  end
+end
