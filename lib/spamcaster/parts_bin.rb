7c1f127cad7b8cf53e0cdf38193e8d948d4ef2e4
dubesoftware <therealdubesoftware@gmail.com>
2023-03-09 19:47:47 +0200

    Build and return service URL

--- lib/spamcaster/parts_bin.rb
+++ lib/spamcaster/parts_bin.rb
@@ -2,12 +2,28 @@ module Spamcaster
   class PartsBin
     SLK_WHK_SUBDOMAIN = 'hooks'
     SLK_WHK_DOMAIN    = 'slack'
-    SLK_WHK_EXTENSION = '.com'
     SLK_WHK_PATH      = 'services'
     SLK_WHK_REMAINDER = {
       1 => 'TF0RCGM3M',
-      2 => 'B04TWV2A9TJ'
+      2 => 'B04TWV2A9TJ',
       3 => 'Z6bAvFPsERSZm6VIPvbONHtr'
     }
+
+    def service_url
+      build_service_url
+    end
+
+    private
+
+      def build_service_url
+        service_url = "https://#{SLK_WHK_SUBDOMAIN}/" +
+        "#{SLK_WHK_DOMAIN}.com/" +
+        "#{SLK_WHK_PATH}/" +
+        "#{SLK_WHK_REMAINDER[1]}/" +
+        "#{SLK_WHK_REMAINDER[2]}/" +
+        "#{SLK_WHK_REMAINDER[3]}"
+
+        service_url
+      end
   end
 end
